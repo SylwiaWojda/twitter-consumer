ded8f194dfd817d0da2d9b03f40032e6fd3e0944
SylwiaWojda <sylwiaaa001@wp.pl>
2022-12-07 15:38:42 +1100

    WIP working consumer which consume tweets array

---
+++ src/main/java/com/kafka/customdeserializers/TweetDeserializer.java
@@ -0,0 +1,21 @@
+package com.kafka.customdeserializers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.IOException;
+
+public class TweetDeserializer implements Deserializer<Tweet[]> {
+
+    @Override
+    public Tweet[] deserialize(String s, byte[] data) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Tweet[] order = null;
+        try {
+            order = objectMapper.readValue(data, Tweet[].class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return order;
+    }
+}
