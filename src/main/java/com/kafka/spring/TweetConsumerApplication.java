e25823bf58824f6cd285c961d8133b6f88f65432
SylwiaWojda <sylwiaaa001@wp.pl>
2022-12-20 16:07:52 +1100

    WIP refactoring. Works

--- src/main/java/com/kafka/spring/TweetConsumerApplication.java
+++ src/main/java/com/kafka/spring/TweetConsumerApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -14,6 +20,31 @@ public class TweetConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TweetConsumerApplication.class, args);
+
+
+
+
+        Process process = null;
+        try {
+            process = new ProcessBuilder()
+                    .command("/Users/iGD-Dev1/Downloads/kafka_2.12-3.3.1/bin/kafka-consumer-groups.sh", "--bootstrap-server", "localhost:9092",
+                            "--describe", "--group", "tweet-group")
+                    .start();
+
+
+            InputStream inputStream = process.getInputStream();
+            try {
+                process.waitFor(60, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            FileCopyUtils.copy(inputStream, baos);
+            System.out.println("LAG: " + new String(baos.toByteArray()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
 }
--- src/main/java/com/kafka/spring/TweetConsumerService.java
+++ src/main/java/com/kafka/spring/TweetConsumerService.java
@@ -2,12 +2,7 @@ package com.kafka.spring;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import org.springframework.util.FileCopyUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class TweetConsumerService {
@@ -16,27 +11,6 @@ public class TweetConsumerService {
     public void consumerTwitterData(Tweet[] tweets) {
         for(Tweet tweet: tweets) {
             System.out.println("Tweet " + tweet.getRaw());
-        }
-
-        Process process = null;
-        try {
-            process = new ProcessBuilder()
-                    .command("/Users/iGD-Dev1/Downloads/kafka_2.12-3.3.1/bin/kafka-consumer-groups.sh", "--bootstrap-server", "localhost:9092",
-                            "--describe", "--group", "tweet-group")
-                    .start();
-
-
-            InputStream inputStream = process.getInputStream();
-            try {
-                process.waitFor(10, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            FileCopyUtils.copy(inputStream, baos);
-            System.out.println("LAG: " + new String(baos.toByteArray()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
 
     }
